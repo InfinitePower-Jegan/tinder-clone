4e553aea75d7d5a3d515f28aa9d9ff6eb67449c8
João Neves <joaopedroneves987@gmail.com>
2023-01-27 01:19:54 +0000

    oh manaia burro esta merda n deixa meter icons rip

--- app/Topheader.tsx
+++ app/Topheader.tsx
@@ -10,6 +10,8 @@ function Topheader() {
         </img>
         <h3>PNG BABY</h3>
       </div>
+      <img width="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaULG0WAOf_HG6VxAKwNSnTlX_VuGklJqlsz91XKY&s"></img>
+      <img width="50px" src="https://w7.pngwing.com/pngs/1000/494/png-transparent-computer-icons-suitcase-baggage-briefcase-suitcase-rectangle-logo-suitcase.png"></img>
     </div>
   )
 }
